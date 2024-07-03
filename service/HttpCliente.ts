155c0137afcc310cc72cac82f3971c82a0e0f362
Daniel Salazar <dsnile.salazr@gmail.com>
2024-07-03 10:02:53 -0600

    chore: Add HttpCliente module for HTTP requests

---
+++ service/HttpCliente.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { AccessTokenRequest } from "expo-auth-session";
+import {REACT_APP_BASE_URL} from '@env';
+const getToken = async()=>{
+    const token = await AsyncStorage.getItem('token');
+    return token;    
+}
+
+axios.defaults.baseURL = REACT_APP_BASE_URL;
+
+axios.interceptors.request.use( async (config)=>{
+    const token = await getToken()
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+        console.log('config', config);
+        return config
+    }
+    return config;
+}, (error)=>{
+    return Promise.reject(error);
+})
+
+
+const requestGenerico = {
+    get : (url:string) => axios.get(url),
+    post : (url:string, body:any) => axios.post(url, body)
+
+}
+
+export default requestGenerico;
