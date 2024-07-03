7c3868efeeed56cfefa939cf5575b4020a67c674
Daniel Salazar <dsnile.salazr@gmail.com>
2024-07-03 10:03:16 -0600

    feat: Add lazy loading for images in perfil tab

--- app/(tabs)/perfil.tsx
+++ app/(tabs)/perfil.tsx
@@ -6,8 +6,12 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect } from 'react';
 
 export default function TabTwoScreen() {
+  
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
