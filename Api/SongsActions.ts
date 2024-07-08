e1a114e57c458e5e470d6e7ca9c8ce6ff61d2cfa
Daniel Salazar <dsnile.salazr@gmail.com>
2024-07-07 23:40:47 -0600

    feat: Agregar carga perezosa de imágenes al componente Card

--- Api/SongsActions.ts
+++ Api/SongsActions.ts
@@ -1,72 +1,76 @@
-import { Axios, AxiosResponse } from 'axios';
-import HttpCliente from '../service/HttpCliente';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Axios, AxiosResponse } from "axios";
+import HttpCliente from "../service/HttpCliente";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+export const getTop = (
+  type: string,
+  offset: number = 0
+): Promise<AxiosResponse<any>> => {
+  return new Promise((resolve, reject) => {
+    HttpCliente.get(`/me/top/${type}?offset=${offset}&time_range=medium_term`)
+      .then((response: AxiosResponse) => {
+        resolve(response.data);
+      })
+      .catch((e: any) => {
+        console.log("ErrorGetTop", e);
+        resolve(e);
+      });
+  });
+};
 
-export const getTop = ( type:string, offset:number = 0,) : Promise<AxiosResponse<any>> =>{
-    return new Promise((resolve, reject) => {
-        HttpCliente.get(`/me/top/${type}?offset=${offset}&time_range=long_term`)
-                    .then((response:AxiosResponse)=>{
-                        resolve(response.data);
-                    })
-                    .catch((e:any) => {
-                        console.log('ErrorGetTop',e);
-                        resolve(e);
-                    })
-        
-    })
+export const getRecomendations = async (): Promise<any> => {
+  const geners_seed = await AsyncStorage.getItem("seedGeneros");
+  const seed_songs = await AsyncStorage.getItem("seedTrack");
+  console.log('seed geners', geners_seed);
+  console.log('seedSongs', seed_songs);
+ const more_recommendations = await getCombinedRecommendations();
 
-}
+  return new Promise((resolve, reject) => {
+    HttpCliente.get(
+      `/recommendations?seed_tracks=${seed_songs}&seed_genres=${geners_seed}&min_popularity=40`
+    )
+      .then((response: any) => {
+        const combined = { ...more_recommendations  ,...response.data.tracks };
+        const result = Object.values(combined);
+        resolve(result);
+      })
+      .catch((e: any) => {
+        console.log(e);
+      });
+  });
+};
 
+export const getListOfSongs = (
+  tracks: string[]
+): Promise<AxiosResponse<any>> => {
+  return new Promise((resolve, reject) => {
+    HttpCliente.get(`/tracks?ids=${tracks}`)
+      .then((response: AxiosResponse) => {
+        resolve(response.data.tracks);
+      })
+      .catch((e: any) => {
+        resolve(e);
+      });
+  });
+};
 
-export const getRecomendations = async () : Promise<any>=>{
-    const geners_seed = await AsyncStorage.getItem('seedGeneros');
-    console.log('generos seed',geners_seed);
-    const seed_songs = await AsyncStorage.getItem('seedTrack');
-    console.log('seed Song', seed_songs)
-    const more_recommendations = await getCombinedRecommendations();
-    const leng = Object.values(more_recommendations);
-    console.log('combinedLength:', leng.length)
-
-
-    return new Promise((resolve, reject) =>{
-        HttpCliente.get(`/recommendations?seed_tracks=${seed_songs}&seed_genres=${geners_seed}&min_danceability=0.4&min_energy=0.4&min_popularity=60`)
-                    .then((response:any)=>{
-                        const combined = {...more_recommendations, ...response.data.tracks}
-                        const result = Object.values(combined);
-                        console.log('RESULT:', result, result.length)
-                        resolve(result);
-                    })
-                    .catch((e:any)=>{
-                        console.log(e);
-                    })
-    })
-}
-
-
-
-export const getListOfSongs = (trakcs : string[]) : Promise<AxiosResponse<any>> => {
-    return new Promise((resolve, reject)=>{
-        HttpCliente.get(`/tracks?ids=${trakcs}`)
-                    .then((response : AxiosResponse)=>{
-                        resolve(response.data.tracks);
-                    })
-                    .catch((e:any)=>{
-                        resolve(e)
-                    })
-    })
-}
-
-
-
- async function getCombinedRecommendations() {
+async function getCombinedRecommendations() {
   try {
     const recommendations_seedGeneros = await getRecommendationsSeedGeneros();
     const recommendations_seedSongs = await getRecommendationsSeedSongs();
     const recommendations_seedArtists = await getRecommendationsSeedArtist();
-    console.log('recomendation length', recommendations_seedGeneros.length, recommendations_seedSongs.length, recommendations_seedArtists.length);
+    console.log(
+      "recomendation length",
+      recommendations_seedGeneros.length,
+      recommendations_seedSongs.length,
+      recommendations_seedArtists.length
+    );
 
-    const result = {...recommendations_seedGeneros, ...recommendations_seedSongs, ...recommendations_seedArtists};
+    const result = {
+      ...recommendations_seedGeneros,
+      ...recommendations_seedSongs,
+      ...recommendations_seedArtists,
+    };
     //const result = Object.values(combined)
     return result;
   } catch (error) {
@@ -74,56 +78,57 @@ export const getListOfSongs = (trakcs : string[]) : Promise<AxiosResponse<any>> 
   }
 }
 
+export const getRecommendationsSeedGeneros = async (): Promise<any> => {
+  const geners_seed = await AsyncStorage.getItem("seedGeneros");
+  console.log("generos seed", geners_seed);
 
-export const getRecommendationsSeedGeneros = async () : Promise<any>=>{
-    const geners_seed = await AsyncStorage.getItem('seedGeneros');
-    console.log('generos seed',geners_seed);
+  return new Promise((resolve, reject) => {
+    HttpCliente.get(
+      `/recommendations?seed_genres=${geners_seed}&min_danceability=0.4&min_energy=0.2&min_popularity=50`
+    )
+      .then((response: any) => {
+        resolve(response.data.tracks);
+      })
+      .catch((e: any) => {
+        resolve([]);
+        console.log(e);
+      });
+  });
+};
 
+export const getRecommendationsSeedSongs = async (): Promise<any> => {
+  const seed_songs = await AsyncStorage.getItem("seedLongTrack");
+  console.log("seed Song", seed_songs);
 
+  return new Promise((resolve, reject) => {
+    HttpCliente.get(
+      `/recommendations?seed_tracks=${seed_songs}&min_popularity=20`
+    )
+      .then((response: any) => {
+        resolve(response.data.tracks);
+      })
+      .catch((e: any) => {
+        resolve([]);
+        console.log(e);
+      });
+  });
+};
 
-    return new Promise((resolve, reject) =>{
-        HttpCliente.get(`/recommendations?seed_genres=${geners_seed}&min_danceability=0.4&min_energy=0.4&min_popularity=60`)
-                    .then((response:any)=>{
-                        resolve(response.data.tracks);
-                    })
-                    .catch((e:any)=>{
-                        console.log(e);
-                    })
-    })
-}
+export const getRecommendationsSeedArtist = async (): Promise<any> => {
+  const seed_songs = await AsyncStorage.getItem("seedArtists");
+  console.log("artistSeed", seed_songs);
 
-
-
-export const getRecommendationsSeedSongs= async () : Promise<any>=>{
-    const seed_songs = await AsyncStorage.getItem('seedLongTrack');
-    console.log('seed Song', seed_songs)
-
-
-    return new Promise((resolve, reject) =>{
-        HttpCliente.get(`/recommendations?seed_tracks=${seed_songs}&min_danceability=0.4&min_popularity=60`)
-                    .then((response:any)=>{
-                        resolve(response.data.tracks);
-                    })
-                    .catch((e:any)=>{
-                        console.log(e);
-                    })
-    })
-}
-
-
-
-export const getRecommendationsSeedArtist= async () : Promise<any>=>{
-    const seed_songs = await AsyncStorage.getItem('seedArtists');
-
-
-    return new Promise((resolve, reject) =>{
-        HttpCliente.get(`/recommendations?seed_artists=${seed_songs}&min_popularity=60`)
-                    .then((response:any)=>{
-                        if(response.data.tracks.length === 0) console.log('no hay canciones')
-                        resolve(response.data.tracks);
-                    })
-                    .catch((e:any)=>{
-                        console.log(e);
-                    })
-    })
-}
+  return new Promise((resolve, reject) => {
+    HttpCliente.get(
+      `/recommendations?seed_artists=${seed_songs}`
+    )
+      .then((response: any) => {
+        if (response.data.tracks.length === 0) console.log("no hay canciones");
+        resolve(response.data.tracks);
+      })
+      .catch((e: any) => {
+        resolve([]);
+        console.log(e);
+      });
+  });
+};
--- app/(app)/(tabs)/music.tsx
+++ app/(app)/(tabs)/music.tsx
@@ -1,85 +1,78 @@
-import { Button, RefreshControl, SafeAreaView, ScrollView, Text, View } from "react-native";
-import React, { Component, useCallback, useEffect, useRef, useState } from "react";
-import { ThemedView } from "@/components/ThemedView";
+import { RefreshControl, SafeAreaView, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
 import { ThemedText } from "@/components/ThemedText";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { styles } from "@/Styles/styles";
-import { Audio } from "expo-av";
-import { useFocusEffect, useNavigation } from "expo-router";
-import { useIsFocused } from "@react-navigation/native";
 import { getListOfSongs, getRecomendations } from "@/Api/SongsActions";
 import Card from "@/components/Card";
 import Animated, { useAnimatedRef } from "react-native-reanimated";
+import { SwipeCard } from "@/components/SwipeCard";
 
 export default function music() {
-   const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const [refreshing, setRefreshing] = useState(false);
-  const [data, setData] = useState([
-    {
-        id:'',
-      name: "",
-      image: "",
-      artist: "",
-      preview_url: "",
-    },
-  ]);
-
-
-  const onRefresh = useCallback(() => {
-
-        fetchData()
-    
-  }, [refreshing]);
-
-  const fetchData = async () => {
-      const response = await getRecomendations();
-      console.log(response.data.tracks.length)
-      let extractedData = response.data.tracks
-        .map((track: any) => ({
-          id: track.id,
-          name: track.name,
-          image: track.album.images[0]?.url || null,
-          artist: track.artists[0]?.name || "",
-          preview_url: track.preview_url || null,
-        } ));
-
-        
-      extractedData = extractedData.filter((i: any) => i.preview_url !== null );
-      console.log(extractedData.length);
-      // console.log(extractedData);
-
-      setData(extractedData);
-    };
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
-    
-    fetchData();
+    if (data.length <= 10) {
+      fetchData().then((traks: any) =>
+        setData((prev: any) => [...prev, ...traks])
+      );
+      console.log(data.length);
+    }
+  }, [data]);
 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchData().then((tracks: any) => {
+      setData(tracks);
+      setRefreshing(false);
+    });
   }, []);
+
+  const fetchData = async () => {
+    const data_response: any = await getRecomendations();
+    console.log('dara response',data_response.length)
+    const to_process = data_response
+      .filter((i: any) => i.preview_url === null)
+      .map((i: any) => i.id)
+      .toString();
+      console.log('to process', to_process.split(',').length)
+    const new_Data : any = await getListOfSongs(to_process);
+    console.log('new data', new_Data.filter((i:any)=>i.preview_url !== null).length);
+    const data_merge = { ...new_Data, ...data_response };
+    const data_result = Object.values(data_merge);
+    const extractedData = data_result.map((track: any) => ({
+      id: track.id,
+      name: track.name,
+      image: track.album.images[0]?.url || null,
+      artist: track.artists[0]?.name || "",
+      preview_url: track.preview_url,
+    })).filter((i:any)=>i.preview_url !== null);
+    console.log(extractedData.length);
+    return extractedData;
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Animated.ScrollView    refreshControl={
-          <RefreshControl  refreshing={refreshing} onRefresh={onRefresh} />
+      <Animated.ScrollView
+        style={{ margin: 0, padding: 0 }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-        ref={scrollRef} scrollEventThrottle={16}>
-
-        
-        <View style={styles.container}>
-          <ThemedView style={styles.textContainer}>
-            <ThemedText type="title">Music</ThemedText>
-          </ThemedView>
-          <View style={styles.container}>
-
+        ref={scrollRef}
+        scrollEventThrottle={16}
+      >
+        {data.length >= 0 ? (
+          <View style={{ marginTop: 40 }}>
+            <SwipeCard items={data} setItems={setData}>
+              {(item: any, swipe: any, isFirst: any) => (
+                <Card swipe={swipe} card={item} isFirst={isFirst} />
+              )}
+            </SwipeCard>
           </View>
-
-          {data ? (
-            data.map((item: any) => <Card card={item} />)
-          ) : (
-            <Text>none</Text>
-          )}
-        </View>
-        
+        ) : (
+          <ThemedText type="subtitle">No Data</ThemedText>
+        )}
       </Animated.ScrollView>
     </SafeAreaView>
   );
