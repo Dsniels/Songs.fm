f022a75aa20ad6eddd97cf41f5a8d27dbc1eefdb
Daniel Salazar <dsnile.salazr@gmail.com>
2024-08-17 19:02:28 -0600

    refactor: Add RangePicker component for selecting date range

---
+++ components/RangePicker.tsx
@@ -0,0 +1,43 @@
+import { View, Text } from "react-native";
+import React, { Dispatch, SetStateAction } from "react";
+import { ThemedText } from "./ThemedText";
+import { Picker } from "@react-native-picker/picker";
+
+const RangePicker = ({
+	selectDate,
+	setSelectDate,
+}: {
+	selectDate: string;
+	setSelectDate: Dispatch<SetStateAction<string>>;
+}) => {
+	return (
+		<>
+			<ThemedText type="defaultSemiBold">Range</ThemedText>
+			<Picker
+				dropdownIconColor="white"
+				mode="dialog"
+				style={{ color: "white", width: 225 }}
+				selectedValue={selectDate}
+				onValueChange={(value) => setSelectDate(value)}
+			>
+				<Picker.Item
+					color="#060C19"
+					label="en el ultimo mes"
+					value={"short_term"}
+				/>
+				<Picker.Item
+					color="#060C19"
+					label="en los ultimos 6 meses"
+					value={"medium_term"}
+				/>
+				<Picker.Item
+					color="#060C19"
+					label="en el ultimo Año"
+					value={"long_term"}
+				/>
+			</Picker>
+		</>
+	);
+};
+
+export default RangePicker;
