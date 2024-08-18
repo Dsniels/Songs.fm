4b57f6903090dfed01d5990fd6ae5c02f4c5a5bd
Daniel Salazar <dsnile.salazr@gmail.com>
2024-08-17 19:02:17 -0600

    feat: Add ListGeners component for displaying genres

---
+++ components/ListGenrs.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { genero } from "@/types/Card.types";
+import { ThemedText } from "./ThemedText";
+
+const ListGeners = ({ item, index }: { item: genero; index: number }) => {
+    
+	return (
+		<View className="m-3 rounded-lg" key={item.name}>
+			<ThemedText type="defaultSemiBold">
+				{index + 1}.- {item.name}
+			</ThemedText>
+			<View className="bg-gray-800 h-4 w-72 rounded-lg">
+				<View
+					className="bg-sky-700 h-full rounded-md"
+					style={{ width: `${item.value * 6}%` }}
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default ListGeners;
