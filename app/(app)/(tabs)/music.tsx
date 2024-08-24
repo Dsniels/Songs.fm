db96c5dd6958c6990e6b3feb52ab2016cd5d645d
Daniel Salazar <dsnile.salazr@gmail.com>
2024-08-24 00:17:26 -0600

    chore: Refactor music component to improve readability and error handling

--- app/(app)/(tabs)/music.tsx
+++ app/(app)/(tabs)/music.tsx
@@ -1,42 +1,51 @@
-import { ActivityIndicator, SafeAreaView, ToastAndroid, View } from "react-native";
+import {
+	ActivityIndicator,
+	SafeAreaView,
+	ToastAndroid,
+	View,
+} from "react-native";
 import { useCallback, useEffect, useState } from "react";
 import { getRecomendations } from "@/Api/SongsActions";
 import Card from "@/components/Card";
 import { SwipeCard } from "@/components/SwipeCard";
 import { Recommendatios, song } from "@/types/Card.types";
 import { loadSongs } from "@/service/loadSongs";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function music() {
 	const [data, setData] = useState<song[]>([]);
 	const [errorCount, setErrorCount] = useState<number>(0);
 
 	const fetchData = useCallback(async (): Promise<void> => {
-		try{
-		const data_response: Recommendatios[] = await getRecomendations();
-		const data_result = data_response.filter(
-			(i: Recommendatios) => i.preview_url !== null
-		);
-		const songs = await loadSongs(data_result);
-		setData((prev)=>[...prev, ...songs]);
-	}catch(_){
-		setErrorCount((prev)=> prev+1);
-		if(errorCount < 3){
-			fetchData();
-		}else{
-			ToastAndroid.show("Ocurrio un error vuelve a intentarlo mas tarde",20 );
-		}
-	}
-		
-	}, []);
+		try {
 
+			const data_response: Recommendatios[] = await getRecomendations();
+			const data_result = data_response.filter(
+				(i: Recommendatios) => i.preview_url !== null
+			);
+			const songs = await loadSongs(data_result);
+			
+			setData((prev) => [...prev, ...songs]);
+		
+		} catch (_) {
+			setErrorCount((prev) => prev + 1);
+			if (errorCount < 3) {
+				fetchData();
+			} else {
+				ToastAndroid.show(
+					"Ocurrio un error vuelve a intentarlo mas tarde",
+					20
+				);
+			}
+		}
+	}, []);
 
 	useEffect(() => {
 		if (data.length <= 10) {
-			fetchData()
+			fetchData();
 		}
 	}, [data]);
 
-  
 	return (
 		<SafeAreaView style={{ backgroundColor: "#000818", flex: 1 }}>
 			{data.length > 0 ? (
